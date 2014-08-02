6bb620e81173e587bb81f48a2c6d80a1c5e3a54f
João Abecasis <joao@abecasis.name>
2014-08-02 20:19:10 +0200

    Working implementation

--- bowling.py
+++ bowling.py
@@ -10,7 +10,45 @@ def calculateScore(rolls):
         '-' indicates a miss
         [1-9] otherwise, indicate the number of knocked pins on a given roll
     """
-    return -1
+
+    runningScore = 0
+
+    # Incremented by 2 for strikes, one for other rolls; divide by 2 for actual
+    # frame count
+    frameCount = 0
+
+    # Keep count of knocked pins for scoring spares
+    previousKnockedPins = 0
+    knockedPins = 0
+
+    # Spares and strikes influence multiplier for subsequent rolls (current,
+    # next and after)
+    rollMultipliers = [1, 1, 1]
+
+    for roll in rolls:
+        frameCount += 1
+        if roll == 'X':
+            frameCount += 1
+            knockedPins = 10
+            if frameCount < 20:
+                rollMultipliers[1] += 1
+                rollMultipliers[2] += 1
+        elif roll == '/':
+            knockedPins = 10 - previousKnockedPins
+            if frameCount < 20:
+                rollMultipliers[1] += 1
+        elif roll == '-':
+            knockedPins = 0
+        else:
+            knockedPins = int(roll)
+
+        multiplier = rollMultipliers.pop(0)
+        rollMultipliers.append(1)
+        previousKnockedPins = knockedPins
+
+        runningScore += multiplier * knockedPins
+
+    return runningScore
 
 
 if __name__ == '__main__':
