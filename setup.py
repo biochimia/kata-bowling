504a43d23067743c4e817698425e07fa899c2984
João Abecasis <joao@abecasis.name>
2014-08-02 19:06:12 +0200

    Initial commit
    MIT License, problem description and setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+# vim: set fileencoding=utf-8 :
+
+from setuptools import setup
+
+setup(
+    name='bowling',
+    description='Calculate the score for American Ten-Pin Bowling',
+
+    author='João Abecasis',
+    author_email='joao@abecasis.name',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    license='MIT',
+)
